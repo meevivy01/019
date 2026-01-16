398dad60d10e492480f1f7e2ed10b4ce8e111363
meevivy01 <meevivy@gmail.com>
2026-01-16 14:56:37 +0700

    Update Git1.py

--- Git1.py
+++ Git1.py
@@ -1633,7 +1633,7 @@ class JobThaiRowScraper:
                                         if EMAIL_USE_HISTORY and person_data['id'] in self.current_history_data:
                                             try:
                                                 last_notify = datetime.datetime.strptime(self.current_history_data[person_data['id']], "%Y-%m-%d").date()
-                                                if (today - last_notify).days < 7: should_add = False
+                                                if (today - last_notify).days < 7: should_add = True
                                             except: pass
                                     if should_add: current_keyword_batch.append(person_data)
 
@@ -1643,7 +1643,8 @@ class JobThaiRowScraper:
                                         if EMAIL_USE_HISTORY and person_data['id'] in self.current_history_data:
                                              try:
                                                   last_notify = datetime.datetime.strptime(self.current_history_data[person_data['id']], "%Y-%m-%d").date()
-                                                  if (today - last_notify).days < 1: should_hot = False
+                                                  if (today - last_notify).days < 1: should_hot = True              
+                                                  #False
                                              except: pass
                                         
                                         if should_hot:
@@ -1691,4 +1692,5 @@ if __name__ == "__main__":
         exit()
     scraper = JobThaiRowScraper()
     scraper.run()
+
 
